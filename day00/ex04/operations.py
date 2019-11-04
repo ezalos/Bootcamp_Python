1c3cae3b5f77ee25f488535235c94b416f216dc6
ezalos <ezalos@gmail.com>
2019-11-04 21:56:20 +0100

    ex 4, init 5

---
+++ day00/ex04/operations.py
@@ -0,0 +1,15 @@
+import sys
+import string
+
+if (len(sys.argv) == 3)
+	a = int(sys.argv[1])
+	b = int(sys.argv[2])
+	print "Sum: " int (a + b)
+	print "Difference: " int (a - b)
+	print "Product: " int (a * b)
+	print "Quotient: " int (a / b)
+	print "Remainder: " int (a % b)
+	Difference: 7
+	Product: 30
+	Quotient: 3.3333333333333335
+	Remainder: 1
